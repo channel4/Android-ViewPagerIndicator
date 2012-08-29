240546a75d5e1d3146769925e9034949bc43c61a
Matt Oakes <matt@ribot.co.uk>
2012-08-29 13:28:55 +0100

    We dont want a max tab width

--- library/src/com/viewpagerindicator/TabPageIndicator.java
+++ library/src/com/viewpagerindicator/TabPageIndicator.java
@@ -95,16 +95,7 @@ public class TabPageIndicator extends HorizontalScrollView implements PageIndica
         final boolean lockedExpanded = widthMode == MeasureSpec.EXACTLY;
         setFillViewport(lockedExpanded);
 
-        final int childCount = mTabLayout.getChildCount();
-        if (childCount > 1 && (widthMode == MeasureSpec.EXACTLY || widthMode == MeasureSpec.AT_MOST)) {
-            if (childCount > 2) {
-                mMaxTabWidth = (int)(MeasureSpec.getSize(widthMeasureSpec) * 0.4f);
-            } else {
-                mMaxTabWidth = MeasureSpec.getSize(widthMeasureSpec) / 2;
-            }
-        } else {
-            mMaxTabWidth = -1;
-        }
+        mMaxTabWidth = -1; // We dont want a max tab width
 
         final int oldWidth = getMeasuredWidth();
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
